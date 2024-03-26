84d83d7822a022d0e98ac976ea2a497c62c24476
letekoma Yao <67656129+letyae@users.noreply.github.com>
2024-03-26 23:53:18 +0100

    Add files via upload
    Toutes les combinaisons possible avec allCombinaisons

---
+++ testadn.java
@@ -0,0 +1,46 @@
+package coding_game_ADN;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class testadn {
+
+	public boolean match(List<String> chaine1, List<String> chaine2) {
+
+		return false;
+	}
+
+	public List<List<Integer>> allCombinaisons(int nb, List<Integer> passed, int n) {
+
+		// System.out.println("**********************_Iteration_*****************************");
+
+		List<List<Integer>> res = new ArrayList<>();
+
+		if (n == 0) {
+			// System.out.println("singleton" + Collections.singletonList(passed));
+			return Collections.singletonList(passed);
+		}
+		for (int i = 0; i < nb; i++) {
+			if (passed.contains(i)) {
+				continue;
+			}
+			List<Integer> passedNext = new ArrayList<>(passed);
+			// System.out.println("passedNext init:" + passedNext);
+			passedNext.add(i);
+			// System.out.println("passedNext init add i:" + passedNext);
+			res.addAll(allCombinaisons(nb, passedNext, n - 1));
+			// System.out.println("res = " + res);
+		}
+		return res;
+	}
+
+	public static void main(String[] args) {
+		List<Integer> passed = new ArrayList<>();
+		testadn adn = new testadn();
+		List<List<Integer>> combinaison = adn.allCombinaisons(2, passed, 2);
+		System.out.println(combinaison);
+
+		List<String> combinaisList = new ArrayList<>();
+	}
+}
